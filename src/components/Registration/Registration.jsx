b9ddfcebfb045be7338e11615b9ce0648b82aad2
NF RIDOY <nfridoy@gmail.com>
2023-10-07 17:33:01 +0600

    Feat: Reg design set

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -2,6 +2,39 @@
 
 export default function Registration() {
     return (
-        <div>Registration</div>
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Registration Now!</h1>
+                        <p className="py-6">
+                        Our dedicated support team is available to assist you 24/7.
+                        </p>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" placeholder="password" className="input input-bordered" required />
+                                {/* <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label> */}
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-success text-white">Registration</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
